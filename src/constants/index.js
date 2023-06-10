407e9cb82862b019555bc0784ffb19d939c9a116
Sithum Raveesha <sithum.20210380@iit.ac.lk>
2023-06-10 21:47:48 +0530

    Updated the work flow

--- src/constants/index.js
+++ src/constants/index.js
@@ -63,3 +63,21 @@ export const feedback = [
         img: people03,
     },
 ];
+
+export const stats = [
+    {
+        id: "stats-1",
+        title: "User Active",
+        value: "3800+",
+    },
+    {
+        id: "stats-2",
+        title: "Truste by Company",
+        value: "230+",
+    },
+    {
+        id: "stats-3",
+        title: "Transaction",
+        value: "$230M+",
+    }
+];
