3eb48c2cc289cacf63695167332840a9ee8ef9cb
Sithum Raveesha <sithum.20210380@iit.ac.lk>
2023-06-11 00:07:52 +0530

    updated the inedx.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -81,3 +81,111 @@ export const stats = [
         value: "$230M+",
     }
 ];
+
+export const  footerLinks = [
+    {
+        title: "Useful Links",
+        links: [
+            {
+                name: "Content",
+                link: "https://www.hoobank.com/content/"
+            },
+            {
+                name: "How it works",
+                link: "https://www.hoobank.com/how-it-works/",
+            },
+            {
+                name: "Create",
+                link: "https://www.hoobank.com/create/",
+            },
+            {
+                name: "Explore",
+                link: "https://www.hoobank.com/explore/",
+            },
+            {
+                name: "Terms & Services",
+                link: "https://www.hoobank.com/terms-and-services/",
+            },
+        ],
+    },
+    {
+        title: "Community",
+        links : [
+            {
+                name: "Help Center",
+                link: "https://www.hoobank.com/help-center/",
+            },
+            {
+                name: "Partners",
+                link: "https://www.hoobank.com/partners/",
+            },
+            {
+                name: "Suggestions",
+                link: "https://www.hoobank.com/suggestions/",
+            },
+            {
+                name: "Blog",
+                link: "https://www.hoobank.com/blog/",
+            },
+            {
+                name: "Newsletters",
+                link: "https://www.hoobank.com/newsletters/",
+            },
+        ],
+    },
+    {
+        title: "Partner",
+        links: [
+            {
+                title: "Our partner",
+                link: "https://www.hoobank.com/our-partner/",
+            },
+            {
+                title: "Become a Partner",
+                link: "https://www.hoobank.com/become-a-partner/",
+            },
+        ],
+    },
+];
+
+export const socialMedia = [
+    {
+        id: "social-media-1",
+        icon: instagram,
+        link: "https://www.instagram.com/",
+    },
+    {
+        id: "social-media-2",
+        icon: facebook,
+        link: "https://www.facebook.com/",
+    },
+    {
+        id: "social-media-3",
+        icon: twitter,
+        links: "https://www.twitter.com/",
+    },
+    {
+        id: "social-media-4",
+        icon: linkedin,
+        link: "https://www.linkedin.com/",
+    },
+];
+
+export const clients = [
+    {
+        id: "client-1",
+        logo: airbnb,
+    },
+    {
+        id: "client-2",
+        logo: binance,
+    },
+    {
+        id: "client-3",
+        logo: coinbase,
+    },
+    {
+        id: "client-4",
+        logo: dropbox,
+    },
+];
