8085234741c3a0ce8e41a593782e01903500abd9
Sithum Raveesha <sithum.20210380@iit.ac.lk>
2023-06-22 13:00:52 +0530

    Updated the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import styles from "./style";
 import videoBackground from './assets/landing_video.mp4';
-import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar,  Stats, Testimonials, Hero } from "./components";
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Stats, Testimonials, Hero } from "./components";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
@@ -9,11 +9,11 @@ const App = () => (
     </video>
     <div className="overlay"></div>
 
-    <div className={`navbar-wrapper ${styles.paddingX} ${styles.flexCenter}` } >
+    {/* <div className={`navbar-wrapper ${styles.paddingX} ${styles.flexCenter}` } >
     <div className={`${styles.boxWidth}`}>
       <Navbar />
       </div>
-    </div>
+    </div> */}
 
     <div className={`navbar-wrapper ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
