e1f1c98e8eeda431a3dd99d4bca89d6e4764c5e6
Sithum Raveesha <85311920+sithum20210380@users.noreply.github.com>
2024-06-23 20:00:21 +0530

    fix: Update Business.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -25,7 +25,6 @@ const Business = () => {
         With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. 
         But with hundreds of credit cards on the market.
         </p>
-
         <Button styles="mt-10"/>
       </div>
       <div className={`${layout.sectionImg} flex-col`}>
